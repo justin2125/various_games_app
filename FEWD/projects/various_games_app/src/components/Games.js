dc6301571f6b33cd884df6743732f6963dc4c69d
Justin Lall <justinlall@icloud.com>
2023-02-01 22:26:36 -0500

    added card

--- FEWD/projects/various_games_app/src/components/Games.js
+++ FEWD/projects/various_games_app/src/components/Games.js
@@ -1,17 +1,19 @@
 const Games = (props) => {
   return (
-    <div class="game">
+    <div>
       <img src={props.image} alt="game pics"></img>
-      <h3>Title: {props.title}</h3>
-      <h3>Year: {props.year}</h3>
-      <h3>Genre: {props.genre}</h3>
-      <h3>
-        Creator: {props.gender}/{props.firstname}/{props.lastname}
-      </h3>
-      <h3>Home: {props.homenumber}</h3>
-      <h3>Personal: {props.personalnumber}</h3>
+      <div class="card">
+        <h3>Title: {props.title}</h3>
+        <h3>Year: {props.year}</h3>
+        <h3>Genre: {props.genre}</h3>
+        <h3>
+          Creator: {props.gender}/{props.firstname}/{props.lastname}
+        </h3>
+        <h3>Home: {props.homenumber}</h3>
+        <h3>Personal: {props.personalnumber}</h3>
 
-      <h3>Age: {props.age}</h3>
+        <h3>Age: {props.age}</h3>
+      </div>
     </div>
   )
 }

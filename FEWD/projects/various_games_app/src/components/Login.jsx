5f1d81ccb49b3e54d3dcee5aee364025cb2010eb
Justin Lall <justinlall@icloud.com>
2023-02-04 15:51:02 -0500

    updated login info

--- FEWD/projects/various_games_app/src/components/Login.jsx
+++ FEWD/projects/various_games_app/src/components/Login.jsx
@@ -1,10 +1,18 @@
+import React, { useState } from "react";
 const Login = () => {
+  const [Email, setEmail] = useState('');
+  const [Pass, setPass] = useState('');
+  const handleSubmit = () => {
+    e.preventDefault()
+    console.log(Email)
+  }
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <label for='Email'>Email: </label>
-      <input type='Email' placeholder='blahblah@aol.com' id='Email' name='Email'/>
+      <input value={Email} onChange={(e)}type='Email' placeholder='blahblah@aol.com' id='Email' name='Email'/>
       <label for='Password'>Password: </label>
-      <input type='Password' placeholder='**********' id='Password' name='Password'/>
+      <input   value={Pass} onChange={(e)} type='Password' placeholder='**********' id='Password' name='Password'/>
+      <button>Login</button>
     </form>
   )
 }
